9dff02c8f6d7824c67516c809fcbb76a724e9833
AshanMaduwantha <ashanofficial256@gmail.com>
2025-03-17 22:36:36 +0530

    create user logout

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -95,3 +95,19 @@ export const login = async (req, res) => {
     }
 
 }
+
+
+export const logout = async (req, res) => {
+    try {
+        res.clearCookie('token', {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict'
+        });
+        
+        return res.json({ success: true, message: 'Logged out' });
+    }
+    catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
+}
