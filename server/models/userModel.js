c9ab63514d28f7daaa6ab1290c32d7e2b14a4282
AshanMaduwantha <ashanofficial256@gmail.com>
2025-03-17 17:12:07 +0530

    create user model

---
+++ server/models/userModel.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    verfyOtp: {
+        type: String,
+        default: ''
+    },
+    verfyOtpExpireAt: {
+        type: Number,
+        default: 0
+    },
+    isAccountVerified: {
+        type: Boolean,
+        default: false
+    },
+    resetOtp: {
+        type: Number,
+        default: ''
+    },
+    resetOtpExpireAt: {
+        type: Number,
+        default: 0
+    },
+});
